58e33f5c02d3b2c5fc2bc111466c12c15069b189
sh2004prvz <115880989+sh2004prvz@users.noreply.github.com>
2022-11-19 19:46:28 +0500

    Add files via upload

---
+++ boolean7.py
@@ -0,0 +1,8 @@
+def bool7(A,B,C):
+    x=True
+    y=False
+    if B>A and C>B:
+        return x
+    else:
+        return y
+print(bool7(1,5,3)) 
---
+++ boolean9.py
@@ -0,0 +1,8 @@
+def bool9(A,B):
+    x=True
+    y=False
+    if A%2==1 or B%2==1:
+        return x
+    else:
+        return y
+print(bool9(2,2)) 
